acfb5947f0dc27cec77480d712d8af536797381a
MaxBased <hoseintahan84@gmail.com>
2024-06-11 23:43:08 +0330

    Update       parser.ts

--- parser.ts
+++ parser.ts
@@ -1,4 +1,5 @@
-import { Stmt, Program, NumericLiteral, Expr, Identifire, VaribleExpr, EqualExpr, VaribleLiteral } from "./ast.ts";
+import { number } from "yup";
+import { Stmt, Program, NumericLiteral, Expr, Identifire, VaribleExpr, EqualExpr, VaribleLiteral, FunctionCaller, BinaryExpr, LogExpr } from "./ast.ts";
 import { tokenize, Token, TokenType } from "./lexer.ts";
 
 
@@ -48,15 +49,21 @@ export default class Parser {
 
 
     private parse_expr(): Expr {
-       if(this.at().type==TokenType.Let || this.at().type==TokenType.Const) {
-        return this.parse_varible_expr();
-       }
-       else if(this.at().type==TokenType.Number){
-        return this.parse_additive_expr();
-       }
-       else{
-        return this.parse_additive_expr();
-       }
+        if (this.at().type == TokenType.Let || this.at().type == TokenType.Const) {
+            return this.parse_varible_expr();
+        }
+        else if (this.at().type == TokenType.Number) {
+            return this.parse_additive_expr();
+        }
+        else if (this.at().type == TokenType.Func) {
+            return this.parse_function_expr();
+        }
+        else if (this.at().type == TokenType.Log) {
+            return this.parse_log_expr();
+        }
+        else {
+            return this.parse_function_expr();
+        }
     }
 
     //  10 + 12
@@ -81,7 +88,96 @@ export default class Parser {
         return left;
     }
 
+    private parse_function_expr() {
+        let name;
+        let body: Expr[] = [];
+        let params = new Array<string>;
+
+        // remove KEYWORD func
+        this.eat();
+
+        name = this.parse_primary_expr();
+
+        if (this.at().value.startsWith('(')) {
+
+            // remove openParen
+            this.eat();
+
+            // get first param
+            params.push(this.eat().value);
+
+            while (this.at().value == ',') {
+                // remove ',' charecter
+                this.eat();
+                params.push(this.eat().value);
+            }
+            // remove closeParen
+            this.eat();
+        }
+        if (this.at().value.startsWith('{')) {
+
+            // remove openBracket
+            this.eat();
+
+            while (this.at().value != '}') {
+
+                if (this.at().type == TokenType.Number) {
+                    body.push(this.parse_additive_expr());
+                }
+                else if (this.at().type == TokenType.Let || this.at().type == TokenType.Const) {
+                    body.push(this.parse_varible_expr());
+                }
+                else if (this.at().type == TokenType.Log) {
+                    body.push(this.parse_log_expr());
+                }
+            }
+
+            // remove closeBracket
+            this.eat();
+        }
+
+        // func asd(a,b,c,d,e)
+        // func asd(a,b,c,d,e){ let a =12  (12+12+32) }
+        // func asd(a,b,c,d,e){ let a =12  12+12+3 }
+        return { kind: "FunctionCaller", name, params, body } as FunctionCaller;
+    }
+
+    // log('1','2')  log('1'+'2')  log(1,2)  log(1+2)
+    private parse_log_expr() {
+
+        let params: Array<any> = [];
+
+        // remove KEYWORD log
+        this.eat();
+
+        if (this.at().type == TokenType.OpenParen) {
+            // remove openParen
+            this.eat();
+
+            const left = this.parse_primary_expr();
+            // check string and number type
+            params.push(left.kind == "Identifire" ? (left['symbol']) : left['value']);
+
+            while (this.at().value == ',' || this.at().value == '+' || this.at().value == '-' || this.at().value == '*' || this.at().value == '/') {
+                const operator = this.eat().value;
+                params.push(operator);
+                const right = this.parse_primary_expr();
+                // check string and number type
+                params.push(right.kind == "Identifire" ? (right['symbol']) : right['value']);
+            }
+
+            //  log('1'+'2',2+'1'+'3','1'+'2'+'3',1+2+3)
+        }
+
+        // remove closeParen
+        this.eat();
+
+        // log('1','2')
+        return { kind: "LogExpr", params } as LogExpr;
+    }
+
     private parse_varible_expr() {
+
         let type, name, operator, value;
         type = this.parse_primary_expr();
         name = this.eat().value;
@@ -102,9 +198,7 @@ export default class Parser {
         const tk = this.at().type;
 
         switch (tk) {
-
             case TokenType.Identifier:
-
                 return {
                     kind: "Identifire",
                     symbol: this.eat().value
