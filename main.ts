0f3c0ad25e7626d74ba645fc40d7e93cf4fc9ca9
MaxBased <hoseintahan84@gmail.com>
2024-07-16 11:29:34 +0330

    Update      main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,22 @@
 import Parser from "./parser.ts";
 import { valueComputing } from "./function/value-computing.ts";
 
-import * as fs from 'node:fs';
-
-repl();
-
-
 async function repl() {
-
     console.log('HoseinTahanLanguage v0.1');
 
     const parser = new Parser();
-
     const memory = parser.memory;
 
-    const input = fs.readFileSync('./runner.htl', 'utf8');
+    const input = await Deno.readTextFile('./runner.htl');
 
     const program = parser.produceAST(input);
-
     program.body.forEach(programAST => {
-
         const result = valueComputing(programAST, memory);
-
-        if (JSON.stringify(result) != '{}')
+        if (JSON.stringify(result) !== '{}') {
             console.log(result);
-
+        }
     });
 
 }
+
+repl();
