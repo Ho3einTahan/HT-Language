42920fc423e308294a32ac977d5b796bd85dc323
MaxBased <hoseintahan84@gmail.com>
2024-07-19 02:25:55 +0330

    Update        memory-func.ts

--- memory/memory-func.ts
+++ memory/memory-func.ts
@@ -1,14 +1,23 @@
+import { Expr } from "../ast/ast.ts";
+import { MemoryVAR } from "./memory-var.ts";
+
+
+export type FuncType = {
+    type: String,
+    params: Array<any>,
+    body: Array<any>,
+};
 
 // Function Memory
 export class MemoryFUNC {
 
-    private memory: Record<string, any> = {};
+    private memory: Record<string, FuncType> = {};
 
     public get_FUNC_VALUE(key: string) {
         return this.memory[key];
     }
 
-    public define_FUNCTION(key: string, value: any, type: string): void {
+    public define_FUNCTION(key: string,value: FuncType): void {
         // this.memory['vName'] = value;
         this.memory[key] = value;
     }
