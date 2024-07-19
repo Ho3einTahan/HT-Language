473c38890a5bab49feeb56768057e56ec7971752
MaxBased <hoseintahan84@gmail.com>
2024-07-19 23:04:30 +0330

    Update      conditional-expr.ts

--- expr/conditional-expr.ts
+++ expr/conditional-expr.ts
@@ -3,10 +3,7 @@ import { TokenType } from "../lexer/lexer.ts";
 import { Expr, conditionalExpr } from "../ast/ast.ts";
 import { parse_varible_expr } from "./varible-expr.ts";
 
-
-
 export function parse_conditional_expr(parser: Parser) {
-
     let params: Array<string> = [];
     let body: Array<Expr> = [];
 
@@ -17,76 +14,89 @@ export function parse_conditional_expr(parser: Parser) {
     parser.eat();
 
     while (parser.at().type != TokenType.CloseParen) {
+
         if (parser.memoryVAR.get(parser.at().value))
             params.push(parser.memoryVAR.get(parser.eat().value));
         else
             params.push(parser.eat().value);
+
     }
 
     // remove closeParen
     parser.eat();
 
+    let paramResult: boolean = eval(params.join(''));
+
     // remove openBracket
     parser.eat();
 
-    while (parser.at().type != TokenType.CloseBracket) {
+    // set count of openBrackets
+    let openBrackets = 1;
 
-        // if this.at().value exist in memory update varible value
-        if (parser.at().type == TokenType.Let || parser.at().type == TokenType.Const || parser.memoryVAR.get(parser.at().value))
-            // update varible value
+    while (openBrackets > 0) {
+
+        if (parser.at().type == TokenType.OpenBracket) {
+            openBrackets++;
+        } else if (parser.at().type == TokenType.CloseBracket) {
+            openBrackets--;
+            if (openBrackets == 0) break;
+        }
+
+        if (paramResult == false) parser.eat();
+
+        // If this.at().value exists in the variable, the value of the variable is updated
+        else if (parser.at().type == TokenType.Let || parser.at().type == TokenType.Const || parser.memoryVAR.get(parser.at().value))
             parse_varible_expr(parser);
-
         else
             body.push(parser.parse_expr());
+
 
     }
 
     // remove closeBracket
     parser.eat();
 
-    let paramResult: boolean = eval(params.join(''));
-
     if (parser.at().type == TokenType.ELSE) {
 
-        // remove ELSE KEYWORD
+        // remove ELSE KEYWWORD
         parser.eat();
 
         // remove openBracket
         parser.eat();
 
-        // clear body for new tokens in else
-        if (paramResult == false) {
-            body = [];
-        }
 
-        while (parser.at().type != TokenType.CloseBracket) {
+        if (paramResult == false) body = [];
 
-            // if top condition is false
+
+        openBrackets = 1;
+
+        while (openBrackets > 0) {
+
+            if (parser.at().type == TokenType.OpenBracket) {
+                openBrackets++;
+            } else if (parser.at().type == TokenType.CloseBracket) {
+                openBrackets--;
+                if (openBrackets == 0) break;
+            }
+
             if (paramResult == false) {
+
                 if (parser.at().type == TokenType.Let || parser.at().type == TokenType.Const || parser.memoryVAR.get(parser.at().value))
-                    // update varible value or define varible
                     parse_varible_expr(parser);
 
                 else
                     body.push(parser.parse_expr());
 
-            } else {
-                // remove extra tokens
+            } else
                 parser.eat();
-            }
 
         }
 
         // remove closeBracket
         parser.eat();
 
-        if (paramResult == false)
-            paramResult = true;
-
+        if (paramResult == false) paramResult = true;
     }
-
-    if (paramResult == false)
-        body = [];
 
     return { kind: "ConditionalExpr", params, paramResult, body } as conditionalExpr;
 }
