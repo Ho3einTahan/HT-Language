8e33c258e170db2bde072594793a25a1bb293557
MaxBased <hoseintahan84@gmail.com>
2024-07-19 17:40:55 +0330

    Update     additive-expr.ts

--- expr/additive-expr.ts
+++ expr/additive-expr.ts
@@ -1,22 +1,44 @@
+import { BinaryExpr, Expr } from "../ast/ast.ts";
 import Parser from "../parser.ts";
 
-//  10 + 12
-   export function parse_additive_expr(parser:Parser) {
 
-        let left, right, operator;
+export function parse_additive_expr(parser: Parser): Expr {
+    let left = parse_multiplicative_expr(parser);
 
-        left = parser.parse_primary_expr();
-
-        while (parser.at().value == '+' || parser.at().value == '-' || parser.at().value == '*' || parser.at().value == '/') {
-            operator = parser.eat().value;
-
-            right = parser.parse_primary_expr();
-
-            left = {
-                kind: "BinaryExpr",
-                left, operator, right
-            };
-
-        }
-        return left;
+    while (parser.not_eof() && parser.at().value === '+' || parser.at().value === '-' ) {
+        const operator = parser.eat().value;
+        const right = parse_multiplicative_expr(parser);
+        left = {
+            kind: "BinaryExpr",
+            left,
+            operator,
+            right
+        } as BinaryExpr;
     }
+
+    
+    return left;
+}
+
+
+
+function parse_multiplicative_expr(parser: Parser) {
+    
+    let left = parser.parse_primary_expr();
+
+    while (parser.not_eof() && (parser.at().value === '*' || parser.at().value === '/')) {
+
+        const operator = parser.eat().value;
+
+        const right = parser.parse_primary_expr();
+
+        left = {
+            kind: "BinaryExpr",
+            left,
+            operator,
+            right
+        } as BinaryExpr;
+    }
+
+    return left;
+}
