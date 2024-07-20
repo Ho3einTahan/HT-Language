fdda2b6bfc4ecdab91d1628952c310e75772e749
MaxBased <hoseintahan84@gmail.com>
2024-07-20 08:49:37 +0330

    Update      [ clean code ]         additive-expr.ts

--- expr/additive-expr.ts
+++ expr/additive-expr.ts
@@ -3,27 +3,26 @@ import Parser from "../parser.ts";
 
 
 export function parse_additive_expr(parser: Parser): Expr {
+
     let left = parse_multiplicative_expr(parser);
 
-    while (parser.not_eof() && parser.at().value === '+' || parser.at().value === '-' ) {
+    while (parser.not_eof() && parser.at().value === '+' || parser.at().value === '-') {
+
         const operator = parser.eat().value;
+
         const right = parse_multiplicative_expr(parser);
-        left = {
-            kind: "BinaryExpr",
-            left,
-            operator,
-            right
-        } as BinaryExpr;
+
+        left = { kind: "BinaryExpr", left, operator, right } as BinaryExpr;
+
     }
 
-    
     return left;
 }
 
 
 
 function parse_multiplicative_expr(parser: Parser) {
-    
+
     let left = parser.parse_primary_expr();
 
     while (parser.not_eof() && (parser.at().value === '*' || parser.at().value === '/')) {
@@ -32,12 +31,8 @@ function parse_multiplicative_expr(parser: Parser) {
 
         const right = parser.parse_primary_expr();
 
-        left = {
-            kind: "BinaryExpr",
-            left,
-            operator,
-            right
-        } as BinaryExpr;
+        left = { kind: "BinaryExpr", left, operator, right } as BinaryExpr;
+
     }
 
     return left;
