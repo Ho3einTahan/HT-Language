33096ae52221ba3afba4c0ede0c7490a300f10b0
MaxBased <hoseintahan84@gmail.com>
2024-07-21 09:12:03 +0330

    Update          [ add   memoryList ]       main.ts

--- main.ts
+++ main.ts
@@ -2,19 +2,22 @@ import Parser from "./parser.ts";
 import { valueComputing } from "./function/value-computing.ts";
 
 async function repl() {
+    
     console.log('HoseinTahanLanguage v0.1');
 
     const parser = new Parser();
+
     const memoryVAR = parser.memoryVAR;
     const memoryFUNC = parser.memoryFUNC;
+    const memoryLIST = parser.memoryLIST;
 
     const input = await Deno.readTextFile('./runner.htl');
 
     const program = parser.produceAST(input);
-
+    
     program.body.forEach(programAST => {
 
-        const result = valueComputing(programAST, memoryVAR, memoryFUNC);
+        const result = valueComputing(programAST, memoryVAR, memoryFUNC,memoryLIST);
 
         if (JSON.stringify(result) !== '{}')
             console.log(result);
