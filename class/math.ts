2e909c322f1eef22f190e48bf0186f0a3bfc3445
MaxBased <hoseintahan84@gmail.com>
2024-07-22 10:32:18 +0330

    add              math.ts

---
+++ class/math.ts
@@ -0,0 +1,37 @@
+export class HTL_MATH{
+
+
+    static  power() {
+        
+    }
+    
+
+    static sqrt(){
+
+        
+    }
+
+    /// get sum of numbers
+    static  sum() {
+        
+    }
+    
+    /// get average from numbers
+    static  avg() {
+        
+    }
+    
+    /// get max of between 2 numbers
+    static  max() {
+        
+    }
+    
+    /// get min of between 2 numbers
+    static  min() {
+        
+    }
+
+    
+ 
+
+}
