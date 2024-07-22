9c30030d9198b9b104819b5ce1809e45ded6308b
MaxBased <hoseintahan84@gmail.com>
2024-07-22 10:33:13 +0330

    add           list.ts

---
+++ class/list.ts
@@ -0,0 +1,61 @@
+export class HTL_LIST{
+
+
+     /**
+      * get length of List
+      */
+     length ():number{
+        return 0;
+    }
+    
+    /**
+     * get item By Index from List
+     */
+    static  at(index:number) {
+        
+    }
+    
+    /**
+     * get last item from List
+     */
+    static  lstItem() {
+        
+    }
+
+    /**
+     * get first item from List
+     */
+    static  fstItem() {
+        
+    }
+
+    /**
+     * delete item in List By Index
+     */
+    static  delete_Index(index:number) {
+        
+    }
+    
+    /**
+     * add new Item to List
+     */
+    static  add(item:Object) {
+        
+    }
+    
+    /**
+     * addAll new Items to List
+     * or reblace with previews items
+     */
+    static  addAll(items) {
+        
+    }
+   
+    /**
+     *  replace item by index
+     */
+    static  replce_Index(index:number) {
+        
+    }
+
+}
