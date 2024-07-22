fbb0736866664de62bc2b0d3ed448fcda1591ace
MaxBased <hoseintahan84@gmail.com>
2024-07-22 10:31:38 +0330

    add       file.ts

---
+++ class/file.ts
@@ -0,0 +1,22 @@
+export class HTL_FILE{
+
+
+    static  writeFile(path:String,fileName:String,content:String) {
+        
+    }
+
+    static  readFile(path:String,fileName:String) {
+        
+    }
+
+    static  createFolder(path:String,folderName:String) {
+        
+    }
+
+    static  renameFile(srcName:String,newName:String) {
+        
+    }
+
+
+
+}
