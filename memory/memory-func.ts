f0c95a28ed167a2ca07c40625e88351b9d474236
MaxBased <hoseintahan84@gmail.com>
2024-07-25 09:07:47 +0330

    Update     memory-func.ts

--- memory/memory-func.ts
+++ memory/memory-func.ts
@@ -7,17 +7,28 @@ export type FuncType = {
 // Function Memory
 export class MemoryFUNC {
 
+    private static instance: MemoryFUNC;
+    
+    private constructor() { }
+
+    public static getInstance(): MemoryFUNC {
+        if (!MemoryFUNC.instance) {
+            MemoryFUNC.instance = new MemoryFUNC();
+        }
+        return MemoryFUNC.instance;
+    }
+
     private memory: Record<string, FuncType> = {};
 
     public get_FUNCTION_VALUE(key: string) {
         return this.memory[key];
     }
-    
+
     public isExist(key: string) {
         return this.memory[key];
     }
 
-    public define_FUNCTION(key: string,value: FuncType): void {
+    public define_FUNCTION(key: string, value: FuncType): void {
         // this.memory['fName'] = value;
         this.memory[key] = value;
     }
