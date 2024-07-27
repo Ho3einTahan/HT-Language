dff8fad2c9ac8c4556d1281ed83c85909584b394
MaxBased <hoseintahan84@gmail.com>
2024-07-27 08:46:34 +0330

    Update             memory-enum.ts

--- memory/memory-enum.ts
+++ memory/memory-enum.ts
@@ -3,19 +3,20 @@
 export class MemoryENUM {
 
     private static instance: MemoryENUM;
-    
+
     private constructor() { }
 
     public static getInstance(): MemoryENUM {
-        if (!MemoryENUM.instance) {
-            MemoryENUM.instance = new MemoryENUM();
-        }
+
+        if (!MemoryENUM.instance) MemoryENUM.instance = new MemoryENUM();
+
         return MemoryENUM.instance;
+        
     }
 
     private memory: Record<string, any> = {};
 
-    public get_ENUM_VALUE(key: string) {
+    public get_VALUE_OF_ENUM(key: string) {
         return this.memory[key];
     }
 
