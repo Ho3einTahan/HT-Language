1b4e0d30a17eed416ddc2ef2893110010461abfd
MaxBased <hoseintahan84@gmail.com>
2024-08-02 22:10:14 +0330

    Update                             list.ts

--- class/list.ts
+++ class/list.ts
@@ -1,61 +1,89 @@
-export class HTL_LIST{
+/// HoseinTahan Language List
+export class HTL_LIST {
 
+    array: Array<any>;
 
-     /**
-      * get length of List
-      */
-     length ():number{
-        return 0;
+    constructor(array: Array<any>) {
+        this.array = array;
+    };
+
+    /**
+     * get length of List
+     */
+    length(): number {
+        return this.array.length;
     }
-    
+
     /**
      * get item By Index from List
+     * @param array
+     * @param index
      */
-    static  at(index:number) {
-        
+    at(array: Array<any>, index: number): any {
+        return array.at(index);
     }
-    
+
+    /**
+     * get index of content
+     * @param content 
+     */
+    getIndex(content: String): number {
+        return this.array.indexOf(content);
+    }
+
     /**
      * get last item from List
      */
-    static  lstItem() {
-        
+    lstItem(): any {
+        return this.array.at(this.array.length - 1);
     }
 
     /**
      * get first item from List
      */
-    static  fstItem() {
-        
+    fstItem(): any {
+        return this.array.at(0);
     }
 
     /**
      * delete item in List By Index
+     * @param index
      */
-    static  delete_Index(index:number) {
-        
+    delete_Index(index: number): Array<any> {
+        delete this.array[index];
+        return this.array;
+        // fix it
     }
-    
+
     /**
      * add new Item to List
+     * @param item
      */
-    static  add(item:Object) {
-        
+    add(item: any): Array<any> {
+        this.array.push(item)
+        return this.array;
     }
-    
+
     /**
      * addAll new Items to List
      * or reblace with previews items
+     * @param items
      */
-    static  addAll(items) {
-        
+    addAll(items: Array<any>): Array<any> {
+        this.array.push(items);
+        return this.array;
     }
-   
+
     /**
      *  replace item by index
+     * @param index
+     * @param content
      */
-    static  replce_Index(index:number) {
-        
+    replce_Index(index: number, content: any) {
+        this.array[index] = content;
+
+        // fix it
+
     }
 
 }
