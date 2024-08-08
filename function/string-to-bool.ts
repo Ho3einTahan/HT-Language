1b2bdf7a4eac42dc8d3e6a3e0a5595b1008a9939
MaxBased <hoseintahan84@gmail.com>
2024-08-09 01:20:45 +0330

    Update               string-to-bool.ts

--- function/string-to-bool.ts
+++ function/string-to-bool.ts
@@ -1,4 +1,12 @@
 
-export function stringToBool(str: string): boolean {
-    return str.toLowerCase() == "true";
+export function stringToBool(str: string): boolean | null {
+    if (str.toLowerCase() == "true") {
+        return true;
+    }
+    else if (str.toLowerCase() == "false") {
+        return false;
+    }
+    else {
+        return null;
+    }
 }
