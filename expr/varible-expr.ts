a6217b4863dc1a758dbed4a1b8e1ff0ad158f611
ho3eintahan <hoseintahan84@gmail.com>
2024-10-28 09:15:41 +0330

    Update     variable-expr.ts

--- expr/varible-expr.ts
+++ expr/varible-expr.ts
@@ -1,7 +1,7 @@
 
 import Parser from "../parser/Parser.ts";
 import { TokenType } from "../lexer/lexer.ts";
-import { Stmt } from "../ast/ast.ts";
+import { Expr, Stmt } from "../ast/ast.ts";
 import { parse_additive_expr } from "./additive-expr.ts";
 import { valueComputing } from "../function/value-computing.ts";
 
@@ -22,9 +22,9 @@ export function parse_varible_expr(parser: Parser) {
     operator = parser.eat().value;
 
     if (parser.memoryVAR.hasVariable(parser.at().value)) {
-        value = parser.memoryVAR.get_VALUE_OF_VARIABLE(parser.eat().value);
+        value = parser.memoryVAR.get_VARIABLE_VALUE(parser.eat().value);
     }
-    else if (parser.memoryLIST.hasList(parser.at().value)) {
+    /* else if (parser.memoryLIST.hasList(parser.at().value)) {
 
         const unValidOperators = ['+', '-', '*', '/', '%'];
         value = parser.memoryLIST.get_BODY_OF_LIST(parser.eat().value).body;
@@ -34,15 +34,18 @@ export function parse_varible_expr(parser: Parser) {
         const MSG = ` '${operator}' cant be applied to types List `;
         if (unValidOperators.includes(parser.at().value)) throw Error(MSG);
 
-    }
+    } */
     else {
+        // EXTRACT AST //
         value = parser.parse_expr();
+        // PASS THE AST TO valueComputing
         const varibleValue = valueComputing(value, parser.memoryVAR, parser.memoryFUNC, parser.memoryLIST);
-        parser.memoryVAR.define_VARIABLE(name, varibleValue, type);
+        parser.memoryVAR.def_VARIABLE(name, varibleValue, type);
         return {} as Stmt;
     }
 
-    parser.memoryVAR.define_VARIABLE(name, value, type);
+    // DEFINE NEW VARIABLE //
+    parser.memoryVAR.def_VARIABLE(name, value, type);
 
-    return {} as Stmt;
+    return {} as Expr;
 }
